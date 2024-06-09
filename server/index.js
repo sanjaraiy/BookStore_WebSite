9c222361ca8e9acaabc957350e0fa8547e7f2a6d
sanjaraiy <deepakrai78788@gmail.com>
2024-06-09 14:12:31 +0530

    added vercel configration file

--- server/index.js
+++ server/index.js
@@ -19,9 +19,12 @@ ConnectToMongoDB(URL);
 
 
 //===============Middleware===========
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
+app.use(cors({
+    origin : [""],
+    credentials : true
+}));
 
 
 //========Define Routers=============
