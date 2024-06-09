d6c51556c8530fed3b67ae746eccdd2fe2a9f7c7
sanjaraiy <deepakrai78788@gmail.com>
2024-06-09 15:13:24 +0530

    updated cors methods

--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
     origin : ["https://book-store-frontend-zeta.vercel.app"],
+    methods : ["GET", "POST"],
     credentials : true
 }));
 
